74248ed3f575b3d01a9769d126a18b93d4e0bb6f
hizoka <kingfly243@hotmail.com>
2022-10-05 10:35:03 +0700

    Add project files.

---
+++ Controllers/WebController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Dpim.Web.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string GenLeftMenu()
+        {
+            StringBuilder strGen = new StringBuilder();
+            strGen.AppendLine("");
+            return strGen.ToString();
+        }
+    }
+}
